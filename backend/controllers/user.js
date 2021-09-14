f52f3d234548cc44eb035a7ffae953d06c4c3fb4
Yoann Perez <yoann.perez@gmail.com>
2021-09-14 15:17:52 +0200

    Finish User's Signup controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,7 +4,7 @@
 // ----------------------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------
 
-'use strict';
+"use strict";
 //--------------------------------------------------
 // Call security modules needed for authentification
 //--------------------------------------------------
@@ -31,47 +31,47 @@ const { Op } = require("sequelize");
 // ------------------------ Création d'un utilisateur ------------------------
 // ---------------------------------------------------------------------------
 
-exports.signup = async (req, res, next) => {
+exports.signup = (req, res, next) => {
   User.findOne({ where: { email: req.body.email } })
-  .then((user) => {
-    // if user doesn't exist in database, return an error
-    if (user) {
-      return res.status(401).json({ error: `Email already exists in database!` });
-    }
-
-  bcrypt
-    // Create an encrypt hash from user's password, salted 10X
-    .hash(req.body.password, 10)
-    // With Promise, create user from userSchema, add email from req, then add hash as password
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      try {
-        const response = User.create({
-          name: req.body.name,
-          email: req.body.email,
-          password: hash,
-        })
-          .then(function (data) {
-            res.status(201).json({ message: "User created" })
-            
-            // return res;
-          })
-          .catch((error) => {
-            const res = { success: false, error: error };
-            return res;
-          });
-        res.status(201).json({ message: "User created" })
-        // res.json(response);
-      } catch (error) {
-        console.log(error);
+    .then((user) => {
+      // if user doesn't exist in database, return an error
+      if (user) {
+        return res.status(401).json({ error: `Email already exists in database!` });
       }
+      bcrypt
+        // Create an encrypt hash from user's password, salted 10X
+        .hash(req.body.password, 10)
+        // With Promise, create user from userSchema, add email from req, then add hash as password
+        .then((hash) => {
+          const user = new User({
+            email: req.body.email,
+            password: hash,
+          });
+          try {
+            const response = User.create({
+              name: req.body.name,
+              email: req.body.email,
+              password: hash,
+            })
+              .then(function (data) {
+                res.status(201).json({ message: "User created" });
+
+                // return res;
+              })
+              .catch((error) => {
+                const res = { success: false, error: error };
+                return res;
+              });
+            res.status(201).json({ message: "User created" });
+            // res.json(response);
+          } catch (error) {
+            console.log(error);
+          }
+        })
+        // if bcrypt has a problem, send an error server status code
+        .catch((error) => res.status(500).json({ error: "Bad request" }));
     })
-    // if bcrypt has a problem, send an error server status code
-    .catch((error) => res.status(500).json({ error: "Bad request" }));
-  });
+    .catch((error) => res.status(500).json({ error: "Very Bad request, need to verify message !" }));
 };
 
 // ----------------------------------------------------------------
@@ -108,46 +108,53 @@ exports.create = async (req, res) => {
 exports.login = (req, res, next) => {
   // Check in database if user exists
   User.findOne({ where: { email: req.body.email } })
-  .then((user) => {
-    // if user doesn't exist in database, return an error
-    if (!user) {
-      return res.status(401).json({ error: `User doesn't exist in database!` });
-    }
-    // if user exists, we need to verify password
-    bcrypt
-      // Bcrypt can compare two different Hashes, and determin if they come from the same password
-      .compare(req.body.password, user.password)
+    .then((user) => {
+      // if user doesn't exist in database, return an error
+      if (!user) {
+        return res.status(401).json({ error: `User doesn't exist in database!` });
+      }
+      // if user exists, we need to verify password
+      bcrypt
+        // Bcrypt can compare two different Hashes, and determin if they come from the same password
+        .compare(req.body.password, user.password)
 
-      .then((valid) => {
-        // In case password is not valid
-        if (!valid) {
-          return res.status(401).json({ error: "Wrong password !" });
-        }
-        // In case password matches with database, we send a response 200, the user id ans the Token created with jsonwebtoken
-        res.status(200).json({
-          userId: user._id,
-          token: jwt.sign(
-            // Token created with userId
-            { userId: user._id },
-            // the private key stored in .env file
-            process.env.TOKEN_KEY,
-            // Valid for 24h
-            { expiresIn: "24h" }
-          ),
-        });
-      })
-      .catch((error) => res.status(500).json({ error: "bcrypt error, check password !" }));
-  })
-  .catch((error) => res.status(500).json({ error :"Server error, POST message invalid, check email !"}));
+        .then((valid) => {
+          // In case password is not valid
+          if (!valid) {
+            return res.status(401).json({ error: "Wrong password !" });
+          }
+          // In case password matches with database, we send a response 200, the user id ans the Token created with jsonwebtoken
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              // Token created with userId
+              { userId: user._id },
+              // the private key stored in .env file
+              process.env.TOKEN_KEY,
+              // Valid for 24h
+              { expiresIn: "24h" }
+            ),
+          });
+        })
+        .catch((error) => res.status(500).json({ error: "bcrypt error, check password !" }));
+    })
+    .catch((error) => res.status(500).json({ error: "Server error, POST message invalid, check email !" }));
 };
+
+// ---------------------------------------------------------------------------
+// -----------------------------  Delete User  -------------------------------
+// ---------------------------------------------------------------------------
+
+
+
+
+
 
 // ---------------------------------------------------------------------------
 // -----------------------  Recherche d'un utilisateur  ----------------------
 // ---------------------------------------------------------------------------
 
-exports.search = (req, res) => {
-
-}
+exports.search = (req, res) => {};
 
 // --------------------------------------------------------------------------
 // -------------------------------  PARKING  --------------------------------

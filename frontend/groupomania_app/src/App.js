394fcc783f5711f37ebed48cdec52e314674c4a4
Yoann Perez <yoann.perez@gmail.com>
2021-09-23 10:37:14 +0200

    Updates done!

---
+++ frontend/groupomania_app/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Mon composant</h1>
+      <br/> 
+      <br/> 
+      <br/> 
+      <form/>
+      <input type="text"/>
+      <button>Envoyer</button>
+        <form/>
+      <div>Coucou</div>
+    </div>
+    
+  );
+}
+
+export default App;

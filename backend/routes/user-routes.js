e2be7bd1bea3eba7d4cc227d45dca35855e019aa
Yoann Perez <yoann.perez@gmail.com>
2021-09-26 22:42:46 +0200

    Another Step in Merging

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,6 +1,25 @@
+//                   -------------------------------------------------------
+//                   --                  ROUTES FOR USERS                 --
+//                   -------------------------------------------------------
+// -----------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
+
+// // Call Middlewares
+// const auth = require("../middleware/auth");
+// const multer = require("../middleware/multer-config");
+
+// //Import controller
+// const userCtrl = require("../controllers/user");
+
+// // --------------- USERS route -------------------
+// router.get("/", auth, userCtrl.getAll); // 
+// router.get("/:id", auth, userCtrl.getOne); // 
+// router.delete("/delete/:id", auth, userCtrl.delete);
+// router.put("/:id", auth, multer, userCtrl.updateUser);
 
 router.post("/new", (req, res) => {
   db.User.create({
